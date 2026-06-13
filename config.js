da8a5daa3968773d49c1acec95a168a70fcf3527
LowWilson <42088406+LowWilson@users.noreply.github.com>
2026-06-14 00:04:56 +0900

    Update config.js

--- config.js
+++ config.js
@@ -3,5 +3,5 @@
 // 例:
 // const SUPABASE_URL = "https://xxxxxxxxxxxx.supabase.co";
 // const SUPABASE_ANON_KEY = "eyJhbGciOi...";
-const SUPABASE_URL = "";
-const SUPABASE_ANON_KEY = "";
+const SUPABASE_URL = "https://hemtnncfjuhnnrtikumr.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhlbXRubmNmanVobm5ydGlrdW1yIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEzNTg0ODcsImV4cCI6MjA5NjkzNDQ4N30.x5ZFePgA-qrBc8xLCh09EfppERiSSoo6QecQ00nIe-Y";
